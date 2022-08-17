fd30e1ae474d30b05c836c8e274912b382c493dd
lauralvd01 <laura.levraud1@etu.univ-lorraine.fr>
2022-08-18 00:06:45 +0200

    Test2 Version1 automate commencé, display à faire

--- test3/main.c
+++ test3/main.c
@@ -1,12 +1,14 @@
 #include "structures.h"
+
+#include<time.h>
 #include<unistd.h>
+#include<ncurses.h>
 
 #include<stddef.h>
 #include<stdlib.h>
-#include<assert.h>
-#include<time.h>
-#include<ncurses.h>
 #include<string.h>
+
+#include<assert.h>
 
 #define NBREDETAGES 5 /* NBREDETAGES = 5 => 4 étages et 1 rez-de-chaussée */
 #define CAPACITE 3 /* Capatié de l'ascenseur */
@@ -16,13 +18,12 @@
 ListeDePersonnes *ptr_fin;
 Ascenseur *ptr_ascenseur;
 ListeDePersonnes **ptr_enAttente;
-ListeDePersonnes **ptr_satisfaits;
 Immeuble *ptr_immeuble;
 
 
 
 int main() {
-    /** Déclaration du pointeur signalant la fin d'une liste **/
+    /** Création du pointeur signalant la fin d'une liste **/
     ptr_fin = (ListeDePersonnes*)malloc(sizeof(ListeDePersonnes));
     ptr_fin->longueur = 0;
     ptr_fin->tete = NULL;
@@ -32,9 +33,8 @@ int main() {
 
     /*** INITIALISATION ***/
 
-    ListeDePersonnes* enAttente[NBREDETAGES];
-    
     /** Remplissage du tableau des listes de personnes en attente **/
+    ListeDePersonnes* enAttente[NBREDETAGES];
     int etage;
     for(etage=0;etage<NBREDETAGES;etage++){
         enAttente[etage] = (ListeDePersonnes*)malloc(sizeof(ListeDePersonnes)); /* Chaque élément du tableau enAttente est un pointeur vers une liste de personnes*/
@@ -50,7 +50,7 @@ int main() {
     ptr_enAttente = (ListeDePersonnes**)malloc(sizeof(ListeDePersonnes*)*NBREDETAGES);
     ptr_enAttente = enAttente;
 
-    ptr_ascenseur = creerAscenseur(CAPACITE,DEPART,ptr_fin);
+    ptr_ascenseur = creerAscenseur(CAPACITE,DEPART,ptr_fin); /* L'ascenseur est initialisé sans personne dedans */
     ptr_immeuble = creerImmeuble(NBREDETAGES,ptr_ascenseur,ptr_enAttente);
 
 
@@ -76,11 +76,59 @@ int main() {
 
 
     /*** LANCEMENT DE L'AUTOMATE ***/
-    int stop = 1;
-    while(stop == 0){
+    bool run = true;
+    while(run){
+        int input = wgetch(fenetre);
+        if(input == 'q'){
+            run = false;
+        }
+        else{
+            int destination = input - '0'; /*Sans le '0' cela ne traduit pas le bon chiffre */
+            if((0 <= destination) & (destination < NBREDETAGES)){
+
+                /** Activation de l'ascenseur **/
+                wclear(fenetre);
+                char desti[20];
+                sprintf(desti,"Destination : %d ",destination);
+                int taille_dest = (int)strlen(desti);
+                mvwprintw(fenetre,LINES -1,COLS -1-taille_dest,desti);
+                /*displayImmeuble(ptr_immeuble);
+                wrefresh(fenetre);*/
+
+                ptr_ascenseur->destination = destination;
+                /*while(ptr_ascenseur->etageActuel != ptr_ascenseur->destination){
+                    transporter(ptr_ascenseur);
+                }
+
+                sortirDelAscenseur(ptr_ascenseur);
+
+                ptr_immeuble->ascenseur = ptr_ascenseur;
+                wclear(fenetre);
+                displayImmeuble(ptr_immeuble);
+                wrefresh(fenetre);
+
+                entrerDanslAscenseur(ptr_immeuble,ptr_ascenseur);
+
+                ptr_immeuble->ascenseur = ptr_ascenseur;
+                wclear(fenetre);
+                displayImmeuble(ptr_immeuble);
+                wrefresh(fenetre);
+                */
+            }
+        }
     }
 
-    
+    wclear(fenetre);
+    char *msg_fin = "SIMULATION TERMINEE";
+    int taille_fin = (int)strlen(msg_fin);
+    attron(A_BOLD);
+    mvwprintw(fenetre,1, (COLS / 2) - (taille_fin / 2),msg_fin);
+    attroff(A_BOLD);
+    wrefresh(fenetre);
+    sleep(2);
+    wclear(fenetre);
+    clear();
+
     endwin(); /* Termine correctement l'utilisation de ncurses */
 
     /** NETTOYAGE DE LA MEMOIRE **/
